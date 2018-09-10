b1ad30b7a73a6b81ef043849735fe4b42ac0fd3b
marcdowie <marc@verticalstructure.com>
2018-09-10 16:48:11 +0100

    Update mysqlhourlybackup.sh

--- mysqlhourlybackup.sh
+++ mysqlhourlybackup.sh
@@ -8,13 +8,9 @@ MYSQL_USER=root
 MYSQL_PASSWORD=password
 MYSQL_HOST=localhost
 MYSQL_DBNAME=--all-databases
-ENCRYPTPASSWORD=encryptionpassword
-BACKUPSTOKEEP=1
-
-
 BACKUP_DIR=/mysqlbackup/hourly
-OUTPUTFILENAME="`hostname -s`_MYSQLBACKUP_`date +%F_%H-%M-%N`"
-
+ENCRYPTPASSWORD=encryptionpassword
+BACKUPSTOKEEP=3
 
 echo "CHECKING FOR BACKUP DIRECTORY"
 
@@ -26,28 +22,24 @@ echo "BACKUP IN PROGRESS"
 
         mysqldump --host=$MYSQL_HOST --user=$MYSQL_USER --password=$MYSQL_PASSWORD $MYSQL_DBNAME | gzip > $BACKUP_DIR/$OUTPUTFILENAME.gz
 
-################################################################
-#Comment out the next 3 lines if you want encryption disabled.
-################################################################
 echo "ENCRYPTION IN PROGRESS"
 
-	openssl enc -aes-256-cbc -e -in $BACKUP_DIR/$OUTPUTFILENAME.gz -out $BACKUP_DIR/$OUTPUTFILENAME.enc -pass pass:$ENCRYPTPASSWORD
-
-################################################################
-#increase +2 if more than 1 backup is needed
-################################################################
+        openssl enc -aes-256-cbc -e -in $BACKUP_DIR/$OUTPUTFILENAME.gz -out $BACKUP_DIR/$OUTPUTFILENAME.enc -pass pass:$ENCRYPTPASSWORD
 
 echo "DELETING OLD BACKUPS"
+        cd $BACKUP_DIR
+        rm $BACKUP_DIR/*.gz
 
-      cd $BACKUP_DIR
-      rm $BACKUP_DIR/*.gz
-      ls -t1 | tail -n +$(($BACKUPSTOKEEP+1)) | xargs rm *.enc
+DELETEOLDFILES=$(ls -t1 $BACKUP_DIR | tail -n +$(($BACKUPSTOKEEP+1)))
 
+if [ -n "$DELETEOLDFILES" ]; then
+    ls -t1 $BACKUP_DIR | tail -n +$(($BACKUPSTOKEEP+1)) | xargs rm
+fi
 
 ################################################################
 #   Unencrypt backup file using the following
 ################################################################
 
-#openssl enc -aes-256-cbc -d -in dbname.sql.gz.enc -out dbname.gz -pass pass:*encryptionpassword* 
+#openssl enc -aes-256-cbc -d -in dbname.sql.gz.enc -out dbname.gz -pass pass:*encryptionpassword*
 #gunzip -c dbname.gz > dbname.sql
 
