2e0bf0372d599ae086693d3a0e1d5f0c1bebc2db
marcdowie <marc@verticalstructure.com>
2018-09-10 15:29:21 +0100

    Update mysqlhourlybackup.sh

--- mysqlhourlybackup.sh
+++ mysqlhourlybackup.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
-
+################################################################
 #  Variables
+################################################################
 
-BACKUP_DIR=/mysqlbackup/hourly
-MYSQL_USER=*dbuser*
-MYSQL_PASSWORD=*dbpassword*
+MYSQL_USER=root
+MYSQL_PASSWORD=password
 MYSQL_HOST=localhost
 MYSQL_DBNAME=--all-databases
+ENCRYPTPASSWORD=encryptionpassword
+
+BACKUP_DIR=/mysqlbackup/hourly
 OUTPUTFILENAME="`hostname -s`_MYSQLBACKUP_`date +%F_%H-%M-%S`"
-ENCRYPTPASSWORD=*encryptionpassword*
+
 
 echo "CHECKING FOR BACKUP DIRECTORY"
 
@@ -21,9 +24,16 @@ echo "BACKUP IN PROGRESS"
 
         mysqldump --host=$MYSQL_HOST --user=$MYSQL_USER --password=$MYSQL_PASSWORD $MYSQL_DBNAME | gzip > $BACKUP_DIR/$OUTPUTFILENAME.gz
 
+################################################################
+#Comment out the next 3 lines if you want encryption disabled.
+################################################################
 echo "ENCRYPTION IN PROGRESS"
 
 	openssl enc -aes-256-cbc -e -in $BACKUP_DIR/$OUTPUTFILENAME.gz -out $BACKUP_DIR/$OUTPUTFILENAME.enc -pass pass:$ENCRYPTPASSWORD
+
+################################################################
+#increase +2 if more than 1 backup is needed
+################################################################
 
 echo "DELETING OLD BACKUPS"
 
